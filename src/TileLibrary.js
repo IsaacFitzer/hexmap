2a26e610d2c63790b5a63a3c5316afbd3096ff51
justicethejust <87838859+justicethejust@users.noreply.github.com>
2022-05-26 18:13:22 -0400

    Update TileLibrary.js

--- src/TileLibrary.js
+++ src/TileLibrary.js
@@ -103,7 +103,7 @@ class TileLibrary extends React.Component {
         return (
             <div className='dropzone library hidden-print' style={{float: "right", border: '2px solid black', height: "600px", width: "400px"}}>
                 {/* {images code goes there  VVVV} */}
-                <img src={require('./tiles/7deadlygamers.png')} height='80px' width='80px' draggable='true' onDragStart={event => event.dataTransfer.setData('text/plain',null)} alt=''/><img src={require('./tiles/discord.png')} height='80px' width='80px' draggable='true' onDragStart={event => event.dataTransfer.setData('text/plain',null)} alt=''/>
+               <img src={require('./tiles/7deadlygamers.png')} height='80px' width='80px' draggable='true' onDragStart={event => event.dataTransfer.setData('text/plain',null)} alt=''/><img src={require('./tiles/discord.png')} height='80px' width='80px' draggable='true' onDragStart={event => event.dataTransfer.setData('text/plain',null)} alt=''/>
             </div>
         )
     }
