3232132e7c7b38a6d0b54b44dbf85c0e28f4f7fe
IsaacFitzer <56335239+IsaacFitzer@users.noreply.github.com>
2022-06-01 15:47:54 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import './App.css';
+import React from 'react';
 import Hexmap from './Hexmap';
 import TileLibrary from './TileLibrary';
+import TagButtons from './TagButtons'
 
-function App() {
-  return (
-    <div className="App">
-      <Hexmap width={875} height={1150} radius={60}/>
-      <TileLibrary/>
-    </div>
-  );
+window.tagPool = [
+  ['coast', 'land', 'sea'],
+  ['forest'],
+]
+
+window.tileTags = {
+  'Tile_1': 'forest coast',
+  'Tile_2': 'coast',
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {selected: ''}
+  }
+
+  onTagClick(tag) {
+    console.log(tag)
+    if (this.state.selected == tag)
+      this.setState({selected: ''})
+    else
+      this.setState({selected: tag})
+    //window.tagPool.includes()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Hexmap width={875} height={1150} radius={60}/>
+        <div style={{float: "right"}}>
+          <TileLibrary/>
+          <TagButtons onTagClick={this.onTagClick} selected={this.state.selected} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
